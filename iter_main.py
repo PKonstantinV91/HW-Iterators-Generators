fbca73eca6296c6e3815dc594fb2bbb855d5fb51
Konstantin Poluyan <kostya19.20yandex.ru>
2023-01-15 11:00:20 +0500

    Initial commit

---
+++ iter_main.py
@@ -0,0 +1,39 @@
+import flattener
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.spis = flattener.flatten(self.list_of_list)
+        self.count = -1
+        return self
+
+    def __next__(self):
+        self.count += 1
+        if len(self.spis) == self.count:
+            raise StopIteration
+        return self.spis[self.count]
+
+
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_3()
